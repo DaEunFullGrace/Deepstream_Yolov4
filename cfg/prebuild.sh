b30d96e908da1e7428e2c07fc1218b7d2acf4033
Daeun <38378873+pshsdu@users.noreply.github.com>
2021-07-28 17:02:13 +0900

    Update prebuild.sh

--- cfg/prebuild.sh
+++ cfg/prebuild.sh
@@ -23,16 +23,6 @@
 ################################################################################
 
 # Download yolo weights
-# For yolo v2,
-echo "Downloading yolov2 config and weights files ... "
-wget https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov2.cfg -q --show-progress
-wget https://pjreddie.com/media/files/yolov2.weights -q --show-progress
-
-# For yolo v2 tiny,
-echo "Downloading yolov2-tiny config and weights files ... "
-wget https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov2-tiny.cfg -q --show-progress
-wget https://pjreddie.com/media/files/yolov2-tiny.weights -q --show-progress
-
 # For yolo v3,
 echo "Downloading yolov3 config and weights files ... "
 wget https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg -q --show-progress
